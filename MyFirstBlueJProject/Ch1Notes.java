ea50970b154fdb25ff7f2834ceae48390df5b14f
dylancswan <dswanson@stu.naperville203.org>
2022-08-23 15:08:12 -0500

    August23
    Learned bluej and took notes

---
+++ MyFirstBlueJProject/Ch1Notes.java
@@ -0,0 +1,39 @@
+
+/**
+ * Dylan Swanson
+ * 8/23/22
+ * Chapter 1 Notes
+ * Class notes on basic concepts
+ * Who helped me:
+ * 
+ */
+
+public class Ch1Notes
+{
+    /**
+     * This is our code notes
+     *
+     */
+    public static void main(String[] args)
+    {
+        //basic data types
+        double number = 8.9, number2;
+        int x = 9, y, z;
+        boolean choice = false;
+        char letter = 'b';
+        float number3 = 8;
+        long n2 = 3;
+        short n3 = 7;
+        
+        
+        //these are classes and objects
+        //class     ID          memory  construction
+        //name      
+        Integer     n1      =   new     Integer(47);
+        String      word    =   new     String("Dylan");
+        Double      number4 =   new     Double(6.7);
+        
+        
+        System.out.print(word.length());
+    }
+}
